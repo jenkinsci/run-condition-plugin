45e220f3bcddce224f4e9b0902998308b844806b
bap2000 <bap-git@a1.org.uk>
2011-11-09 04:02:17 +0000

    Add BooleanCondition

---
+++ src/main/java/org/jenkins_ci/plugins/run_condition/core/BooleanCondition.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License
+ *
+ * Copyright (C) 2011 by Anthony Robinson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.jenkins_ci.plugins.run_condition.core;
+
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.util.FormValidation;
+import org.jenkins_ci.plugins.run_condition.Messages;
+import org.jenkins_ci.plugins.run_condition.RunCondition;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import java.util.regex.Pattern;
+
+public class BooleanCondition extends RunCondition {
+
+    private static final Pattern RUN_REGEX = Pattern.compile("^(1|y|yes|t|true|on|run)$");
+    final String token;
+
+    @DataBoundConstructor
+    public BooleanCondition(final String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    @Override
+    public boolean runPrebuild(final AbstractBuild<?, ?> build, final BuildListener listener) {
+        return true;
+    }
+
+    @Override
+    public boolean runPerform(final AbstractBuild<?, ?> build, final BuildListener listener) throws Exception {
+        final String expandedToken = Util.fixEmptyAndTrim(TokenMacro.expand(build, listener, token));
+        if (expandedToken == null) return false;
+        return RUN_REGEX.matcher(expandedToken.toLowerCase()).matches();
+    }
+
+    @Extension
+    public static class BooleanConditionDescriptor extends RunConditionDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.booleanCondition_displayName();
+        }
+
+        public FormValidation doCheckToken(@QueryParameter final String value) {
+            return FormValidation.validateRequired(value);
+        }
+
+    }
+
+}
