b106ff85475f7e817c402218207f562e2acd65bd
bap2000 <bap-git@a1.org.uk>
2011-11-09 03:01:38 +0000

    Add a spot of javadoc for the EP

--- src/main/java/org/jenkins_ci/plugins/run_condition/RunCondition.java
+++ src/main/java/org/jenkins_ci/plugins/run_condition/RunCondition.java
@@ -25,7 +25,6 @@
 package org.jenkins_ci.plugins.run_condition;
 
 import hudson.DescriptorExtensionList;
-import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
@@ -33,15 +32,37 @@ import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
 
-import java.io.IOException;
-
+/**
+ * Define a condition that can be used to decide whether to run a BuildStep or not.
+ * 
+ * A Run condition should not make any changes to the build or the build environment.
+ * If the information that is required to make the decision is not available, then the RunCondition should
+ * either explicitly throw an exception (or just allow one to be thrown) rather than handling it and trying to decide whether the build
+ * should run based on bad data. This allows a user to choose what should happen - which could be different in different contexts.
+ */
 public abstract class RunCondition implements Describable<RunCondition>, ExtensionPoint {
 
     public static DescriptorExtensionList<RunCondition, RunConditionDescriptor> all() {
         return Hudson.getInstance().<RunCondition, RunConditionDescriptor>getDescriptorList(RunCondition.class);
     }
 
+    /**
+     * Decide whether to run the prebuild method in the BuildStep.
+     * 
+     * If the condition depends, or may depend, on something that may be affected by the build itself, then it is probably safest to always
+     * return true here. (@TODO review this once the plugin has been in use for a while with a variety of conditions and BuildSteps)
+     * If the condition is not dependent on the state of the build i.e. just looks at the build timestamp, then this method should
+     * return the same as the runPerform method.
+     * 
+     * @return true if the prebuild method of the BuildStep should run
+     */
     public abstract boolean runPrebuild(final AbstractBuild<?, ?> build, final BuildListener listener) throws Exception;
+
+    /**
+     * Decide whether to run the perform method in the BuildStep.
+     * 
+     * @return true if the perform method of the BuildStep should run
+     */
     public abstract boolean runPerform(final AbstractBuild<?, ?> build, final BuildListener listener) throws Exception;
 
     public RunConditionDescriptor getDescriptor() {
